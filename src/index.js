5e1693b38538697787ec1f572b2f22a8caa9fea2
Nadja Brandys <nadiusza@gmail.com>
2020-10-22 21:35:41 +0200

    Day 2-4

---
+++ src/index.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Image from './images/10512dragon_figure.jpg'
+
+const welcome = 'Welcome to 30 Days Of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const firstName = 'Asabeneh'
+const lastName = 'Yetayeh'
+const author = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+}
+const date = 'Oct 3, 2020'
+
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>{firstName}{lastName}</p>
+      <small>{date}</small>
+    </div>
+  </header>
+)
+
+// User Card Component
+const yearBorn = 1820
+const currentYear = 2020
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+const UserCard = () => (
+  <div className='user-card'>
+    <img src={Image} alt='asabeneh image' />
+    <h2>{firstName}{lastName}</h2>
+  </div>
+)
+
+// TechList Component
+
+  const techs = ['HTML', 'CSS', 'JavaScript']
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+
+// Main Component
+const Main = () => (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+       {techsFormatted}
+      </ul>
+      <UserCard />
+    </div>
+  </main>
+)
+
+// Footer Component
+const copyRight = 'Copyright 2020'
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+<p>{copyRight}</p>
+    </div>
+  </footer>
+)
+
+// The App, or the parent or the container component
+const App = () => (
+  <div className='app'>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+// we render the App component using the ReactDOM package
+ReactDOM.render(<App />, rootElement)
