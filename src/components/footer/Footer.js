6622488e54f1fe22efefeac31b78674f41c9ff55
Nadja Brandys <nadiusza@gmail.com>
2020-11-03 13:46:06 +0100

    started working on events

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class Footer extends React.Component {
+    constructor(props) {
+      super(props)
+    }
+    render() {
+      return (
+        <footer>
+          <div className='footer-wrapper'>
+            <p>Copyright {this.props.date.getFullYear()}</p>
+          </div>
+        </footer>
+      )
+    }
+  }
+  export default Footer
