da6a04560fc7642a172aac104894816cabd00a5b
Nadja Brandys <nadiusza@gmail.com>
2020-11-11 23:09:44 +0100

    react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
-// index.js
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
 
- const App = () => <h1>Welcome to 30 Days Of React</h1>
-export default App;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
+
+
+const Home = (props) => <h1>Welcome Home</h1>
+// About component
+const About = (props) => <h1>About Us</h1>
+// Contact component
+const Contact = (props) => <h1>Contact us</h1>
+// Challenge component
+const Challenges = (props) => (
+  <div>
+    <h1>30 Days Of React Challenge</h1>
+  </div>
+)
+
+class App extends React.Component{
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+        <ul>
+            <li>
+              <NavLink to='/'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to='/about'>About</NavLink>
+            </li>
+            <li>
+              <NavLink to='/contact'>Contact</NavLink>
+            </li>
+            <li>
+              <NavLink to='/challenges'>Challenges</NavLink>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/challenges' component={Challenges} />
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </div>
+      </Router>
+
+    )}
+
+}
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
