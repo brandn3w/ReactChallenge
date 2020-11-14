c72fdeb75280d9281f27039703d1b988f4fa3264
Nadja Brandys <nadiusza@gmail.com>
2020-11-14 23:10:23 +0100

    no solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,63 @@
-// index.js
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import axios from 'axios'
 
- const App = () => <h1>Welcome to 30 Days Of React</h1>
-export default App;
+const Cat= ({ cat: {name, weight, lifespan } }) => {
+  return (
+    <div className='cat'>
+      <div className='cat_wrapper'>
+    
+      </div>
+      <h3 className='cat_breed'>{name.toUpperCase()}</h3>
+      <div class='country_text'>
+        <p>
+          <span>Weight:{weight}</span>
+      
+        </p>
+      </div>
+    </div>
+  )
+}
+
+class App extends Component {
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+    this.fetchCatData()
+  }
+  fetchCatData = async () => {
+    const url = 'https://api.thecatapi.com/v1/breeds'
+    try {
+      const response = await axios.get(url)
+      const data = await response.data
+      this.setState({
+        data,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+
+    return (
+      <div className='App'>
+        <h1>React Component Life Cycle</h1>
+        <h1>Calling API</h1>
+        <div>
+          <p>There are {this.state.data.length} cats in the api</p>
+          <div className='countries-wrapper'>
+            {this.state.data.map((cat) => (
+              <Cat cat={cat} />
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
