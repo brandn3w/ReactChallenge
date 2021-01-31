5bfb82adae34957fd8b6c29cef5e2c0afe64c399
Nadja Brandys <nadiusza@gmail.com>
2021-01-31 23:38:31 +0100

    add background color changing by state

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { findRenderedComponentWithType } from 'react-dom/test-utils'
 
 // class base component
 class Header extends React.Component {
@@ -47,10 +48,9 @@ class TechList extends React.Component {
 // Main Component
 // Class Component
 class Main extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+
   render() {
+    const {changeColor} = this.props
     return (
       <main>
         <div className='main-wrapper'>
@@ -58,6 +58,7 @@ class Main extends React.Component {
           <ul>
             <TechList techs={this.props.techs}/>
           </ul>
+          <button onClick={()=>changeColor()}>change color</button>
         </div>
       </main>
     )
@@ -84,8 +85,24 @@ class Footer extends React.Component {
 // The App, or the parent or the container component
 // Class Component
 class App extends React.Component {
+  state ={
+    styles: {
+      backgroundColor: "white",
+      color: "black"}
+  }
+  changeColor = () => {
+    let lightStyle= {styles: {backgroundColor: 'white', color: "black"}}
+    let darkStyle ={styles: {backgroundColor: "black", color:"white"}}
+    let pageStyle = this.state.styles.backgroundColor === "white" ? darkStyle :lightStyle
+    this.setState(pageStyle)
+  }
+
+  style = () => {
+    return this.state.styles;
+  }
 
   render() {
+
     const data = {
         welcome: 'Welcome to 30 Days Of React',
         title: 'Getting Started React',
@@ -98,9 +115,11 @@ class App extends React.Component {
       }
       const techs = ['HTML', 'CSS', 'JavaScript']
     return (
-      <div className='app'>
+      <div className='app' style= {this.state.styles}>
+     
         <Header data={data}/>
-        <Main techs={techs}/>
+        <Main techs={techs} changeColor = {this.changeColor}/>
+     
         <Footer />
       </div>
     )
